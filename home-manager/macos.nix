8a54de976a3f0f1edccce5a0e68a6f180e840efe
André Sá de Mello <asmello@pm.me>
2024-03-10 22:48:15 +0000

    macos upgrade resiliency

--- home-manager/macos.nix
+++ home-manager/macos.nix
@@ -1,6 +1,18 @@
-{ config, pkgs, ... }: import ./common.nix {
-  inherit config pkgs;
-  username = "asm";
-  homeDirectory = "/Users/asm";
+{ lib, config, pkgs, ... }:
+let
+  rendered = import ./common.nix {
+    inherit config pkgs;
+    username = "asm";
+    homeDirectory = "/Users/asm";
+  };
+in lib.attrsets.recursiveUpdate rendered {
+  programs.zsh.initExtra = ''
+    # Nix
+    if [ -e '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh' ]; then
+      . '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh'
+    fi
+    # End Nix
+    source ${pkgs.zsh-vi-mode}/share/zsh-vi-mode/zsh-vi-mode.plugin.zsh
+    bindkey '^[[Z' autosuggest-accept
+  '';
 }
-
